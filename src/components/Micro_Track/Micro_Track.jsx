a2c79ea6bb5e137451f2c3491fddcda4d31e2fb7
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-01-24 16:46:52 +0100

    ✨ multiple preview

--- src/components/Micro_Track/Micro_Track.jsx
+++ src/components/Micro_Track/Micro_Track.jsx
@@ -6,6 +6,7 @@ import gsap from "gsap";
 const Micro_track = ({ data, play, setPlay, id }) => {
 
     const audio = useRef(null);
+    const wrapper = useRef(null);
 
     const handleClick = () => {
         if (play === id) {
@@ -27,10 +28,10 @@ const Micro_track = ({ data, play, setPlay, id }) => {
         if (play === id) {
             if (audio.current.ended) {
                 setPlay(false);
-                // wrapper.current.style.background = "var(--color-primary)";
+                wrapper.current.style.background = "var(--color-neutral-50)";
             } else {
-                // const percent = (audio.current.currentTime / audio.current.duration) * 100;
-                // wrapper.current.style.background = `conic-gradient(var(--color-primary) ${percent}%, var(--color-neutral-800) ${percent}%)`;
+                const percent = (audio.current.currentTime / audio.current.duration) * 100;
+                wrapper.current.style.background = `conic-gradient(var(--color-neutral-50) ${percent}%, var(--color-neutral-800) ${percent}%)`;
             }
         }
     }
@@ -48,12 +49,14 @@ const Micro_track = ({ data, play, setPlay, id }) => {
                 <audio ref={audio} controls>
                     <source src={data?.preview} type="audio/mp3" />
                 </audio>
-                <Micro_PlayBtn handleClick={handleClick} play={play} idTrack={id} />
+                <div ref={wrapper} className={`${s["Micro-Track__btn-wrapper"]} ${play === id && s['Micro-Track__btn-wrapper--play']}`}>
+                    <Micro_PlayBtn handleClick={handleClick} play={play} idTrack={id} />
+                </div>
                 <img className={s["Micro-Track__cover"]} src={data?.album?.cover} alt="" />
             </div>
             <div className={s["Micro-Track__info"]}>
                 <a href={data?.link} target="_blank">
-                    <h2 className={s["Micro-Track__name"]}>{data?.title}</h2>
+                    <h2 className={`${s["Micro-Track__name"]}  ${play === id && s['Micro-Track__name--play']}`}>{data?.title}</h2>
                 </a>
                 <a href={data?.artist?.link} target="_blank" className={s["Micro-Track__artist"]}>{data?.artist?.name}</a>
             </div>
