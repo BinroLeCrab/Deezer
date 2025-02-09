be4f0e6a812d844897d9d83c7181009c842bc0eb
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-02-09 21:13:08 +0100

    📝  Add crédit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 
+console.log(
+    "---------------------\n" +
+    "🚀 Micro-Projet\n" +
+    "💾 Deezer Favorites - Mes coups de coeurs Deezer\n" +
+    "📅 janvier - février 2025\n" +
+    "\n" +
+    "Micro-projet en React.js utilisant l'API Deezer pour afficher ma playlist coup de coeur de l'ajout le plus récent au plus ancien.\n" +
+    "\n" +
+    "Made by : 🧑‍🚀Robin VIGIER\n" +
+    "---------------------\n"
+);
+
 createRoot(document.getElementById('root')).render(
     <App />
 )
